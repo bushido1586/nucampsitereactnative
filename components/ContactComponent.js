041bd2a7dee911bd8b367caa2ca80498bc168e52
Bradley Reed <shenmusha@yahoo.com>
2021-07-24 09:52:33 -0400

    Week 1 assignment

---
+++ components/ContactComponent.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { ScrollView, Text } from 'react-native';
+import { Card } from 'react-native-elements';
+
+class Contact extends Component{
+
+    static navigationOptions = {
+        title: 'Contact Us'
+    }
+    render(){
+        return(
+            <ScrollView>
+                <Card 
+                title="Contact Information"
+                wrapperStyle={{margin: 20}}>
+                    <Text style={{marginBottom: 10}}>
+                        1 Nucamp Way
+                        Seattle, WA 98001
+                        U.S.A. 
+
+                        Phone 1-206-555-1234
+                        Email: campsites@nucamp.co
+                    </Text>
+                </Card>
+            </ScrollView>
+        );
+    }
+}
+
+export default Contact;
