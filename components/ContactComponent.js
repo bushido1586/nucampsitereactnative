044605788ec2a97ad2813c31fac6f0a46eb98d80
Bradley Reed <shenmusha@yahoo.com>
2021-07-24 12:06:57 -0400

    updated componenets

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -14,14 +14,12 @@ class Contact extends Component{
                 <Card 
                 title="Contact Information"
                 wrapperStyle={{margin: 20}}>
-                    <Text style={{marginBottom: 10}}>
-                        1 Nucamp Way{'\n'}
-                        Seattle, WA 98001{'\n'}
-                        U.S.A. {'\n'}
-                        {'\n'}
-                        Phone 1-206-555-1234{'\n'}
-                        Email: campsites@nucamp.co
-                    </Text>
+                    <Text>1 Nucamp Way</Text>
+                    <Text>Seattle, WA 98001</Text>
+                    <Text style={{marginBottom: 10}}>U.S.A.</Text>
+                        
+                    <Text>Phone 1-206-555-1234</Text>
+                    <Text>Email: campsites@nucamp.co</Text>     
                 </Card>
             </ScrollView>
         );
