ccd90c1ab9a41e936e3e46f80938156f6f0ed239
Bradley Reed <shenmusha@yahoo.com>
2021-07-25 21:40:14 -0400

    with image component

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -117,7 +117,7 @@ const CustomDrawerContentComponent = props => (
             forceInset={{top: 'always', horizontal: 'never'}}>
             <View style={styles.drawerHeader}>
                 <View style={{flex: 1}}>
-                <Image source={require('./images/logo.png')} style={styles.drawerImage} />
+                    <Image source={require('./images/logo.png')} style={styles.drawerImage} />
                 </View>
                 <View style={{flex: 2}}>
                     <Text style={styles.drawerHeaderText}>NuCamp</Text>
