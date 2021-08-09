afa18fd0a2cb1c0c0b6147a0bc1fffe9f062cd74
Bradley Reed <shenmusha@yahoo.com>
2021-08-09 16:32:36 -0400

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet,
     Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import * as Animatable from 'react-native-animatable'
+import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
 
@@ -38,8 +39,11 @@ class Reservation extends Component {
                         style: 'cancel'
                     },
                     {
-                        text: 'OK',
-                        onPress: () => this.resetForm()
+                        text: 'OK', 
+                        onPress: () => {
+                            this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                            this.resetForm();
+                        }
                     },
                 ],
                 
@@ -53,6 +57,31 @@ class Reservation extends Component {
             hikeIn: false, 
             showCalender: false, 
         });
+    }
+
+    async presentLocalNotification(date){
+        function sendNotification(){
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                }, 
+                trigger: null
+            });
+        }
+        let permissions = await Notifications.getPermissionsAsync();
+        if(!permissions.granted){
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if(permissions.granted){
+            sendNotification();
+        }
     }
 
     render() {
